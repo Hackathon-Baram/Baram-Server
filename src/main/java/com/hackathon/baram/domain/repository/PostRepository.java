f149a0bf5d4e2c5d4c9f78c2a3d6f85d91818905
godbs129 <junhaeyoon@gmail.com>
2021-12-29 12:53:49 +0900

    conflict

--- src/main/java/com/hackathon/baram/domain/repository/PostRepository.java
+++ src/main/java/com/hackathon/baram/domain/repository/PostRepository.java
@@ -2,16 +2,12 @@ package com.hackathon.baram.domain.repository;
 
 import com.hackathon.baram.domain.entity.PostEntity;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
-
+    
     List<PostEntity> findAll(Sort sort);
 
     PostEntity findByIdx(Long idx);
